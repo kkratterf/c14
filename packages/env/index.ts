6e5a36956be8bf49b74059bfef7344e5a91888dc
Federico Kratter Thaler <kkratterf@gmail.com>
2024-12-26 17:18:49 +0100

    Remove apps & packages

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -2,9 +2,7 @@ import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
 const server: Parameters<typeof createEnv>[0]['server'] = {
-  RESEND_FROM: z.string().min(1).email(),
   DATABASE_URL: z.string().min(1).url(),
-  RESEND_TOKEN: z.string().min(1).startsWith('re_'),
   BETTERSTACK_API_KEY: z.string().min(1).optional(),
   BETTERSTACK_URL: z.string().min(1).url().optional(),
   ARCJET_KEY: z.string().min(1).startsWith('ajkey_').optional(),
@@ -15,8 +13,6 @@ const server: Parameters<typeof createEnv>[0]['server'] = {
       z.string().min(1).startsWith('testsk_'),
     ])
     .optional(),
-  UPSTASH_REDIS_REST_URL: z.string().min(1).url().optional(),
-  UPSTASH_REDIS_REST_TOKEN: z.string().min(1).optional(),
 
   // Added by Sentry Integration, Vercel Marketplace
   SENTRY_ORG: z.string().min(1).optional(),
@@ -26,7 +22,6 @@ const server: Parameters<typeof createEnv>[0]['server'] = {
   VERCEL: z.string().optional(),
   NEXT_RUNTIME: z.enum(['nodejs', 'edge']).optional(),
   FLAGS_SECRET: z.string().min(1).optional(),
-  BLOB_READ_WRITE_TOKEN: z.string().min(1).optional(),
 };
 
 const client: Parameters<typeof createEnv>[0]['client'] = {
@@ -44,9 +39,7 @@ export const env = createEnv({
   client,
   server,
   runtimeEnv: {
-    RESEND_FROM: process.env.RESEND_FROM,
     DATABASE_URL: process.env.DATABASE_URL,
-    RESEND_TOKEN: process.env.RESEND_TOKEN,
     BETTERSTACK_API_KEY: process.env.BETTERSTACK_API_KEY,
     BETTERSTACK_URL: process.env.BETTERSTACK_URL,
     ARCJET_KEY: process.env.ARCJET_KEY,
@@ -56,7 +49,6 @@ export const env = createEnv({
     VERCEL: process.env.VERCEL,
     NEXT_RUNTIME: process.env.NEXT_RUNTIME,
     FLAGS_SECRET: process.env.FLAGS_SECRET,
-    BLOB_READ_WRITE_TOKEN: process.env.BLOB_READ_WRITE_TOKEN,
     SVIX_TOKEN: process.env.SVIX_TOKEN,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     NEXT_PUBLIC_DOCS_URL: process.env.NEXT_PUBLIC_DOCS_URL,
