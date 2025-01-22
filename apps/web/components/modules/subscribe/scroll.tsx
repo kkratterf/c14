15ff890099e7bfda7f76b2faa98afe37e2465cb7
Federico Kratter Thaler <kkratterf@gmail.com>
2025-01-22 21:53:45 +0100

    Error handling

--- apps/web/components/modules/subscribe/scroll.tsx
+++ apps/web/components/modules/subscribe/scroll.tsx
@@ -12,9 +12,7 @@ interface StartupCardProps {
   item: Startup;
 }
 
-const StartupCard = ({
-  item: { name, logo },
-}: StartupCardProps) => {
+const StartupCard = ({ item: { name, logo } }: StartupCardProps) => {
   return (
     <Avatar key={name} className='size-20 rounded-xl border border-border'>
       <AvatarImage src={logo ?? undefined} />
@@ -24,20 +22,29 @@ const StartupCard = ({
 };
 
 const StartupsScroll = async () => {
-  const { startups: scrollStartups } = await getStartups({
-    isPopular: false,
-  });
-  return (
-    <div className='absolute z-0 h-20 w-full'>
-      <Marquee pauseOnHover className="[--duration:20s]">
-        {scrollStartups.map((startup) => (
-          <StartupCard key={startup.name} item={startup} />
-        ))}
-      </Marquee>
-      <div className='absolute top-0 left-0 h-20 w-8 bg-gradient-to-l from-white/0 to-white/100 dark:from-[#1B1D21]/0 dark:to-[#1B1D21]/100' />
-      <div className='absolute top-0 right-0 h-20 w-8 bg-gradient-to-r from-white/0 to-white/100 dark:from-[#1B1D21]/0 dark:to-[#1B1D21]/100' />
-    </div>
-  );
-}
+  try {
+    const { startups: scrollStartups } = await getStartups({
+      isPopular: false,
+    });
+
+    if (!scrollStartups?.length) {
+      return null;
+    }
+
+    return (
+      <div className='absolute z-0 h-20 w-full'>
+        <Marquee pauseOnHover className="[--duration:20s]">
+          {scrollStartups.map((startup) => (
+            <StartupCard key={startup.name} item={startup} />
+          ))}
+        </Marquee>
+        <div className='absolute top-0 left-0 h-20 w-8 bg-gradient-to-l from-white/0 to-white/100 dark:from-[#1B1D21]/0 dark:to-[#1B1D21]/100' />
+        <div className='absolute top-0 right-0 h-20 w-8 bg-gradient-to-r from-white/0 to-white/100 dark:from-[#1B1D21]/0 dark:to-[#1B1D21]/100' />
+      </div>
+    );
+  } catch {
+    return null;
+  }
+};
 
 export default StartupsScroll;
